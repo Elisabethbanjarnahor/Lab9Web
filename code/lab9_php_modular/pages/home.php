abcd950a581bd909c6c03b1e3c469c426903bcbe
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2025-12-02 00:57:51 +0700

    Create home.php

---
+++ code/lab9_php_modular/pages/home.php
@@ -0,0 +1,3 @@
+<h2>Halaman Home</h2>
+<p>Selamat datang di aplikasi Modularisasi PHP (Praktikum 9). 
+Website ini menggunakan konsep header, footer, dan routing agar struktur lebih rapi dan mudah dikelola.</p>
