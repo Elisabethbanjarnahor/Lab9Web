af498e4d5ddee5cb642f4ea30015a5d5069bbf43
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2025-12-02 00:49:16 +0700

    Create heade.php

---
+++ code/lab9_php_modular/heade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Modularisasi PHP - Praktikum 9</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="container">
+    <header>
+        <h1>Modularisasi Menggunakan PHP</h1>
+    </header>
+
+    <nav>
+        <a href="index.php?page=home">Home</a>
+        <a href="index.php?page=about">About</a>
+        <a href="index.php?page=kontak">Kontak</a>
+        <a href="index.php?page=user/list">Data Barang</a>
+    </nav>
+
+    <div class="content">
