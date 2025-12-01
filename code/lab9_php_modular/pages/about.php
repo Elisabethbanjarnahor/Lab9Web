af7943695235f71c68d243439a5baad844b7b184
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2025-12-02 00:57:28 +0700

    Create about.php

---
+++ code/lab9_php_modular/pages/about.php
@@ -0,0 +1,3 @@
+<h2>Halaman About</h2>
+<p>Aplikasi ini dibuat untuk memenuhi Praktikum 9 Pemrograman Web.</p>
+<p>Modularisasi membantu memisahkan tampilan (template) dan konten sehingga kode lebih terstruktur.</p>
