c7467ad41f6475f379732f86c83eac84b444c498
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2025-12-02 00:58:13 +0700

    Create kontak.php

---
+++ code/lab9_php_modular/pages/kontak.php
@@ -0,0 +1,8 @@
+<h2>Halaman Kontak</h2>
+<p>Hubungi kami melalui informasi berikut:</p>
+
+<ul>
+    <li>Email: info@kampus.ac.id</li>
+    <li>WhatsApp: 0812-xxxx-xxxx</li>
+    <li>Alamat: Universitas Pelita Bangsa, Bekasi</li>
+</ul>
